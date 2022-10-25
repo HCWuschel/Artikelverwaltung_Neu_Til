6dd1aafa64461583709d6b54267adc63ba1fb6fd
Til <t.m.rudolstadt@gmail.com>
2022-10-25 10:08:30 +0200

    Projektdateien hinzufügen.

---
+++ Artikelverwaltung_Neu_Til/Form1.cs
@@ -0,0 +1,10 @@
+namespace Artikelverwaltung_Neu_Til
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Artikelverwaltung_Neu_Til/Program.cs
@@ -0,0 +1,15 @@
+namespace Artikelverwaltung_Neu_Til
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
